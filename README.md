33642d550da6f50ccdc4dae1e160d6785b6a4e77
Akshat Gupta <akshat2032003@gmail.com>
2025-11-16 21:30:51 +0530

    Completed the progress bar

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hello
+## Introduction
+This is a normal text. Hello everyone.
+<!-- to start new para in markdown seperate two lines wit one extra line -->
+This is to *tell* y**o**u ~~about~~ my experiene with typing<!--use two spaces to switch to new line without changing para-->  dsfsdfsdff
--- ytdownload.py
+++ ytdownload.py
@@ -1,5 +1,9 @@
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
+from tqdm import tqdm
+
+progress_bar = None
+total_file_size = 0
 
 
 def list_info(yt:YouTube, url:str, audio=False):
@@ -24,17 +28,32 @@ def download(yt:YouTube, audio=False):
     
 def main():
     url=input("url: ")
-    yt=YouTube(url)
+    yt=YouTube(url, on_progress_callback=prog)
     
     list_info(yt, url)
-    download(yt)
+    tqdm(download(yt))
     print("\n")
     list_info(yt, url, True)
-    download(yt, True)
+    tqdm(download(yt, True))
     
     
 
-def prog():
+def prog(stream, chunk, bytes_remaining):
+    global progress_bar, total_file_size
+    
+    if progress_bar is None:
+        total_file_size = stream.filesize
+        progress_bar = tqdm(total=total_file_size, unit="A", unit_scale=True, desc="Progres")
+    
+    progress_bar.update(len(chunk))
+    
+    if bytes_remaining == 0:
+        progress_bar.close()
+        progress_bar = None
+    pass
+
+
+def complete():
     pass
 
 
